c9453d1bebcf1b64c4de4cbe5f5f7b892c9ae697
Isaac Zais <isaaczais@hotmail.com>
2018-07-12 19:19:52 -0500

    Added DM Roller

--- Controllers/RollerController.cs
+++ Controllers/RollerController.cs
@@ -14,7 +14,7 @@ namespace DemiplaneTemplate.Controllers
 
         public RollerController()
         {
-            rollers = new List<IDieRoller>() { new ArbitraryDieAdapter(new RandomDieRoller()), new ArbitraryDieAdapter(new AllOrNothingDieRoller()) };
+            rollers = new List<IDieRoller>() { new ArbitraryDieAdapter(new RandomDieRoller()), new ArbitraryDieAdapter(new AllOrNothingDieRoller()), new ArbitraryDieAdapter(new DMDieRoller()) };
         }
 
         public IActionResult Index()
---
+++ Rollers/DMDieRoller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemiplaneTemplate.Rollers
+{
+    public class DMDieRoller : IArbitraryDieRoller
+    {
+        private Random random = new Random();
+
+        public string Name => "DM Dice";
+        
+        public int RollDx(int x)
+        {
+            var numOfDice = random.Next(1, 4);
+
+            var roll = 0;
+
+            for (int i = 0; i < numOfDice; i++)
+            {
+                roll = Math.Max(roll, this.random.Next(1, x + 1));
+            }
+
+            return roll;
+        }
+    }
+}
