bbfcf2822c4005e0a2ce3fb4c87996c615341b7b
Isaac Zais <isaaczais@hotmail.com>
2018-09-10 17:44:46 -0500

    Added update flash

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d4-result", 0, 300);
+            fadingObject.countDown();
             $('#d4-result').text(result);
         });
     });
@@ -25,6 +27,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d6-result", 0, 300);
+            fadingObject.countDown();
             $('#d6-result').text(result);
         });
     });
@@ -37,6 +41,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d8-result", 0, 300);
+            fadingObject.countDown();
             $('#d8-result').text(result);
         });
     });
@@ -49,6 +55,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d10-result", 0, 300);
+            fadingObject.countDown();
             $('#d10-result').text(result);
         });
     });
@@ -61,6 +69,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d12-result", 0, 300);
+            fadingObject.countDown();
             $('#d12-result').text(result);
         });
     });
@@ -73,6 +83,8 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d20-result", 0, 300);
+            fadingObject.countDown();
             $('#d20-result').text(result);
         });
     });
@@ -86,7 +98,38 @@ $(document).ready(function () {
                 RollerName: rollerName
             }
         }).done(function (result) {
+            fadingObject.fade("d100-result", 0, 300);
+            fadingObject.countDown();
             $('#d100-result').text(result);
         });
     });
 });
+
+
+var fadingObject = {
+    yellowColor: function (val) {
+        var r = (val*1.25).toString(16);
+        var g = (val*1.25).toString(16);
+        var b = (val*1.25).toString(16);
+        var newval = "#" + r + g + b;
+        return newval;
+    },
+
+    fade: function (id, start, finish) {
+        this.count = this.start = start;
+        this.finish = finish;
+        this.id = id;
+        this.countDown = function () {
+            this.count += 30;
+            if (this.count >= this.finish) {
+                document.getElementById(this.id).style.background =
+                    "transparent";
+                this.countDown = null;
+                return;
+            }
+            document.getElementById(this.id).style.backgroundColor =
+                this.yellowColor(this.count);
+            setTimeout(this.countDown.bind(this), 25);
+        }
+    }
+};
